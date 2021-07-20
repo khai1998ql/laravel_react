63d15a839fe40cac0fba3259f171b8d612499e65
Khai <khainguyensi.1998@gmail.com>
2021-07-20 23:42:01 +0700

    '...'

--- resources/js/contants/Config.js
+++ resources/js/contants/Config.js
@@ -1,3 +1,2 @@
 export const NAME_PROJECT = '/laravel_react';
 export const API_URL = 'http://tranninglaravelreact.herokuapp.com/api';
-export const API_URL1 = 'http://tranninglaravelreact.herokuapp.com/api';
--- resources/js/utils/apiCaller.js
+++ resources/js/utils/apiCaller.js
@@ -4,7 +4,7 @@ import * as Config from './../contants/Config';
 export default function callApi(endpoint, method = 'GET', body){
     return axios({
         method : method,
-        url : `${Config.API_URL1}/${endpoint}`,
+        url : `${Config.API_URL}/${endpoint}`,
         data : body
     }).catch(err => {
         console.log(err);
