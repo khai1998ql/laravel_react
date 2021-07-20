1779aaaeee2799865cccc02c01857169a746ea20
Khai <khainguyensi.1998@gmail.com>
2021-07-20 22:47:27 +0700

    '..'

--- resources/js/contants/Config.js
+++ resources/js/contants/Config.js
@@ -1,4 +1,2 @@
 export const NAME_PROJECT = '/laravel_react';
-// export const API_URL = 'http://localhost/laravel_react/api';
 export const API_URL = 'http://tranninglaravelreact.herokuapp.com/api';
-export const NAME_PROJECT11 = '/laravel_react';
